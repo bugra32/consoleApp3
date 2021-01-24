01a18f24fe80cfb02343c677a50d048edf26ae90
buğra <bugra3298@gmail.com>
2021-01-24 21:12:03 +0300

    20 ocak ders sonu

---
+++ Genericsintro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Genericsintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mylisti t bazında verdik istediğimiz türde olsun diye
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
