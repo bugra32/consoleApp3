bf703e3c0aae9124619b4cbcaf093db8a4ea97ac
bugra32 <61611526+bugra32@users.noreply.github.com>
2021-01-25 22:45:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> kisi = new MyDictionary<string, int>();
+            kisi.Add(1, "hako");
+            kisi.Add(2,"maho");
+            kisi.Add(3,"samo");
+
+            Console.WriteLine(kisi.Count);
+        }
+    }
+}
